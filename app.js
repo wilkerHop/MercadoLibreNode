e115df77b74301e38108c68d0f30c53d91355c4e
Florencia Solari <fsolari@MacBook-Pro-2.local>
2017-10-26 17:44:55 -0300

    inlcude stylesheets & logo

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ app.get('/', function (req, res) {
   res.sendFile('index.html', {root: __dirname });
 });
 
+app.use(express.static(__dirname + '/assets'));
+
 app.listen(process.env.PORT || 5000, function () {
   console.log('Example app listening on port 5000!');
 });
