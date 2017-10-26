ad45279c8541832bed9975a19db9c4ff5e9159d9
Florencia Solari <fsolari@MacBook-Pro-2.local>
2017-10-26 17:27:59 -0300

    get examples

--- tests/testget.js
+++ tests/testget.js
@@ -10,7 +10,7 @@ var app = express();
 
 var examples = {
     1: "Get your country's category tree",
-    2: "Get your user private data with access_token",
+    2: "Get user data",
     3: "Get multiple users at once"
 };
 function printOptions() {
@@ -28,24 +28,35 @@ stdin.addListener("data", function (d) {
     var end = d.toString();
 
     if (d == 1) {
-        meliObject.get('sites/' + config.config.site_id +'/categories', function (err, res) {
-            console.log(err, res);
-        });
+    getSites();
     }
     else if (d == 2) {
-        meliObject.get('users/me?access_token=' + config.config.access_token, function (err, res) {
-             console.log(err, res);
-        });
+    getUser();
     }   
     else if (d == 3) {
+        getUsers();
+    }
+});
+
+
+var getSites = function(){
+        meliObject.get('sites/' + config.config.site_id +'/categories', function (err, res) {
+            console.log(err, res);
+});
+    };
+
+var getUser = function(){
+        meliObject.get('users/145925943', function (err, res) {
+             console.log(err, res);
+});
+    };
+
+    var getUsers = function(){
         meliObject.get('users', {
             ids: [145925943, 145925951]
         }, function (err, res) {
             console.log(err, res);
         });
-    }
-}
-});
-
+    };
 
 
